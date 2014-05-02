9e4518d9f3ede67e3f0c956ac14bf060d32f4100
anshul2117 <anshul2102@gmail.com>
2014-05-02 16:01:12 -0700

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
-var mongoose = require('mongoose');
+var mongoose  = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
 
