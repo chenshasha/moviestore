d4ee3d05652a9d5b31062f518d51c73dfcda260e
gauravshiralkar <Grv@Gaurav>
2014-05-03 18:44:05 -0700

    Dashboard

--- app/routes-mysql.js
+++ app/routes-mysql.js
@@ -6,7 +6,7 @@ var mysql = require('../node_modules/mysql');
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : '',
+	password : 'pass',
 	database : 'moviestore'
 });
 
